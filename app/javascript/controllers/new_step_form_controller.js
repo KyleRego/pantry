2d5a875818bfc8ddbad8db0035543962a6323b85
Kyle Rego <regoky@outlook.com>
2024-03-01 10:58:43 -0500

    Add removing recipe step

--- app/javascript/controllers/new_step_form_controller.js
+++ app/javascript/controllers/new_step_form_controller.js
@@ -20,5 +20,24 @@ export default class extends Controller {
     this.numberInputTarget.setAttribute("value", thisStepsNumber);
     this.numberInputTarget.setAttribute("name", `recipe[steps_attributes][${thisStepsNumber}][number]`);
     this.numberInputTarget.setAttribute("id", `recipe_steps_attributes_${thisStepsNumber}_number`);
+
+    this.showOnlyLastRemoveStepButton();
+  }
+
+  removeStep() {
+    this.element.remove();
+    this.showOnlyLastRemoveStepButton();
+  }
+
+  showOnlyLastRemoveStepButton() {
+    let btns = document.querySelectorAll(".remove-step-button")
+    let btnsCount = btns.length;
+    btns.forEach((btn, i) => {
+      if (i !== btnsCount - 1) {
+        btn.classList.add("display-none");
+      } else {
+        btn.classList.remove("display-none");
+      }
+    });
   }
 }
